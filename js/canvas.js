926e83aff6a7bfc37501abd2d9aabc3f6472d6c1
DaGarcia39 <dagarcia282@misena.edu.co>
2022-10-18 16:52:27 -0500

    Se agregaron los cambios del live part2

--- js/canvas.js
+++ js/canvas.js
@@ -20,12 +20,32 @@ function drawLine() {
     board.fillStyle = "#F3F5F6";
     board.strokeStyle = "#0A3871";
 
-    let width = 600 / secretWord.length;
+    let widthLine = 600 / secretWord.length;
     for (let i = 0; i < secretWord.length; i++) {
-        board.moveTo(500 + width * i, 640);
-        board.lineTo(500 + width * i, 640);
+        board.moveTo(500 + widthLine * i, 640);
+        board.lineTo(550 + widthLine * i, 640);
     }
 
     board.stroke();
     board.closePath();
+}
+
+function writeCorrectLetter(index) {
+    board.font = "bold 52px Inter";
+    board.lineWidth = 6;
+    board.lineCap = "round";
+    board.lineJoin = "round";
+    board.fillStyle = "#0A3871";
+    let widthLine = 600 / secretWord.length;
+    board.fillText(secretWord[index], 505 + widthLine * index, 620);
+    board.stroke();
+}
+
+function writeIncorrectLetter(letter, errorsLeft) {
+    board.lineWidth = 6;
+    board.font = "bold 40px Inter";
+    board.lineCap = "round";
+    board.lineJoin = "round";
+    board.fillStyle = "#0A3871";
+    board.fillText(letter, 505 + 40 * (10 - errorsLeft), 720, 40);
 }
--- js/horca.js
+++ js/horca.js
@@ -2,10 +2,33 @@ let words = ["ALURA", "ORACLE", "ONE", "JAVASCRIPT", "HTML", "CSS"];
 let board = document.getElementById("forca").getContext("2d");
 let secretWord = "";
 
+let letters = [];
+let errors = 9;
+
 function chooseCorrectWord() {
     let word = words[Math.floor(Math.random() * words.length)];
     secretWord = word;
     console.log(secretWord);
+}
+
+function checkLetter(key) {
+    let state = false;
+    if (
+        (key >= 65 && letters.indexOf(key)) ||
+        (key <= 90 && letters.indexOf(key))
+    ) {
+        letters.push(key);
+        console.log(key);
+        return state;
+    } else {
+        state = true;
+        console.log(key);
+        return true;
+    }
+}
+
+function addIncorrectLetter() {
+    errors -= 1;
 }
 
 function startGame() {
@@ -13,4 +36,18 @@ function startGame() {
     chooseCorrectWord();
     drawCanvas();
     drawLine();
+
+    document.onkeydown = (e) => {
+        let letter = e.key.toUpperCase();
+        if (checkLetter(letter) && secretWord.includes(letter)) {
+            for (let i = 0; i < secretWord.length; i++) {
+                if (secretWord[i] === letter) {
+                    writeCorrectLetter(i);
+                }
+            }
+        } else {
+            addIncorrectLetter(letter);
+            writeIncorrectLetter(letter, errors);
+        }
+    };
 }
