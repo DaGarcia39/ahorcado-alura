a2bf99d4185bade31c40b2455d1ed3a1ec97dfbd
DaGarcia39 <dagarcia282@misena.edu.co>
2022-10-20 11:31:46 -0500

    Se agrego el funcionanmiento con mejores practicas

--- js/canvas.js
+++ js/canvas.js
@@ -2,10 +2,9 @@ function drawCanvas() {
     board.lineWidth = 8;
     board.lineCap = "round";
     board.lineJoin = "round";
-    board.fillStyle = "#F3F5F6";
+    board.fillStyle = "#F3F5FC";
     board.strokeStyle = "#0A3871";
-
-    board.fillRect(0, 0, 1200, 860);
+    board.fillRect(0, 0, 1200, 800);
     board.beginPath();
     board.moveTo(650, 500);
     board.lineTo(900, 500);
@@ -17,15 +16,13 @@ function drawLine() {
     board.lineWidth = 6;
     board.lineCap = "round";
     board.lineJoin = "round";
-    board.fillStyle = "#F3F5F6";
     board.strokeStyle = "#0A3871";
-
+    board.beginPath();
     let widthLine = 600 / secretWord.length;
     for (let i = 0; i < secretWord.length; i++) {
         board.moveTo(500 + widthLine * i, 640);
         board.lineTo(550 + widthLine * i, 640);
     }
-
     board.stroke();
     board.closePath();
 }
@@ -47,5 +44,74 @@ function writeIncorrectLetter(letter, errorsLeft) {
     board.lineCap = "round";
     board.lineJoin = "round";
     board.fillStyle = "#0A3871";
-    board.fillText(letter, 505 + 40 * (10 - errorsLeft), 720, 40);
+    board.fillText(letter, 535 + 40 * (10 - errorsLeft), 710, 40);
+}
+
+function drawHanged(score) {
+    board.lineWidth = 8;
+    board.lineCap = "round";
+    board.lineJoin = "round";
+    board.strokeStyle = "#0A3871";
+    if (score === 8) {
+        board.moveTo(700, 500);
+        board.lineTo(700, 100);
+    }
+    if (score === 7) {
+        board.moveTo(850, 100);
+        board.lineTo(700, 100);
+    }
+    if (score === 6) {
+        board.moveTo(850, 100);
+        board.lineTo(850, 171);
+    }
+    if (score === 5) {
+        board.moveTo(900, 230);
+        board.arc(850, 230, 50, 0, Math.PI * 2);
+    }
+    if (score === 4) {
+        board.moveTo(850, 389);
+        board.lineTo(850, 289);
+    }
+    if (score === 3) {
+        board.moveTo(850, 389);
+        board.lineTo(800, 450);
+    }
+    if (score === 2) {
+        board.moveTo(850, 389);
+        board.lineTo(890, 450);
+    }
+    if (score === 1) {
+        board.moveTo(850, 330);
+        board.lineTo(800, 389);
+    }
+    if (score === 0) {
+        board.moveTo(850, 330);
+        board.lineTo(890, 389);
+    }
+    board.stroke();
+    board.closePath();
+}
+
+function loseGame() {
+    board.font = "bold 42px Inter";
+    board.lineWidth = 6;
+    board.lineCap = "round";
+    board.lineJoin = "round";
+    board.fillStyle = "red";
+    board.fillText("Fin del juego!", 930, 320);
+}
+
+function winGame() {
+    board.font = "bold 42px Inter";
+    board.lineWidth = 6;
+    board.lineCap = "round";
+    board.lineJoin = "round";
+    board.fillStyle = "green";
+    board.fillText("Ganaste, ", 950, 320);
+    board.fillText("Felicidades!", 930, 360);
+    setTimeout(reload, 1000);
+}
+
+function reload() {
+    location.reload();
 }
--- js/horca.js
+++ js/horca.js
@@ -1,53 +1,145 @@
+let screen = document.querySelector("canvas");
+let buttonNewGame = (document.getElementById("btn-new-game").style.display =
+    "none");
+let btnExitVanish = (document.getElementById("btn-exit").style.display =
+    "none");
+let sectionAddWord = (document.getElementById("add-word").style.display =
+    "none");
+let btnNewGame = document.getElementById("btn-new-game");
+let btnCancel = document.getElementById("btn-cancel");
+let btnExit = document.getElementById("btn-exit");
+
 let words = ["ALURA", "ORACLE", "ONE", "JAVASCRIPT", "HTML", "CSS"];
 let board = document.getElementById("forca").getContext("2d");
 let secretWord = "";
 
 let letters = [];
-let errors = 9;
+let errors = 8;
+let correctWord = "";
+let incorrectLetters = [];
+let numberErrors = 8;
+let chosenLetter = [];
+
+// Events
+document.getElementById("start-game").onclick = () => startGame();
+
+document.getElementById("btn-save").onclick = () => saveWord();
+
+btnNewGame.addEventListener("click", function () {
+    location.reload();
+});
+
+btnExit.addEventListener("click", function () {
+    location.reload();
+});
+
+btnCancel.addEventListener("click", function () {
+    location.reload();
+});
 
 function chooseCorrectWord() {
     let word = words[Math.floor(Math.random() * words.length)];
     secretWord = word;
-    console.log(secretWord);
+    return word;
 }
 
-function checkLetter(key) {
-    let state = false;
-    if (
-        (key >= 65 && letters.indexOf(key)) ||
-        (key <= 90 && letters.indexOf(key))
-    ) {
+function checkLetterClicked(key) {
+    if (letters.length < 1 || letters.indexOf(key) < 0) {
         letters.push(key);
-        console.log(key);
-        return state;
+        return false;
     } else {
-        state = true;
-        console.log(key);
+        letters.push(key);
         return true;
     }
 }
 
-function addIncorrectLetter() {
-    errors -= 1;
+function addCorrectLetter(i) {
+    correctWord += secretWord[i].toUpperCase();
+}
+
+function addIncorrectLetter(letter) {
+    if (secretWord.indexOf(letter) <= 0) {
+        errors -= 1;
+    }
+}
+
+function checkEndGame(letter) {
+    if (chosenLetter.length < secretWord.length) {
+        incorrectLetters.push(letter);
+
+        if (incorrectLetters.length > numberErrors) {
+            loseGame();
+        } else if (chosenLetter.length < secretWord.length) {
+            addIncorrectLetter(letter);
+            writeIncorrectLetter(letter, errors);
+        }
+    }
+}
+
+function checkWinner(letter) {
+    chosenLetter.push(letter.toUpperCase());
+    if (chosenLetter.length == secretWord.length) {
+        winGame();
+    }
+}
+
+function checkLetter(keyCode) {
+    if (typeof keyCode === "number" && keyCode >= 65 && keyCode <= 90) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function showScreenAddWord() {
+    document.getElementById("section-disable").style.display = "none";
+    document.getElementById("add-word").style.display = "block";
+}
+
+function saveWord() {
+    let newWord = document.getElementById("input-new-word").value;
+
+    if (newWord !== "") {
+        words.push(newWord.toUpperCase());
+        alert("La palabra fue guardada");
+
+        document.getElementById("add-word").style.display = "none";
+        startGame();
+    } else {
+        alert("Ninguna palabra ha sido digitada");
+    }
 }
 
 function startGame() {
-    document.getElementById("start-game").style.display = "none";
-    chooseCorrectWord();
+    document.getElementById("section-disable").style.display = "none";
     drawCanvas();
+    chooseCorrectWord();
     drawLine();
+
+    document.getElementById("btn-new-game").style.display = "block";
+    document.getElementById("btn-exit").style.display = "block";
 
     document.onkeydown = (e) => {
         let letter = e.key.toUpperCase();
-        if (checkLetter(letter) && secretWord.includes(letter)) {
-            for (let i = 0; i < secretWord.length; i++) {
-                if (secretWord[i] === letter) {
-                    writeCorrectLetter(i);
+        if (incorrectLetters.length <= numberErrors) {
+            if (!checkLetterClicked(e.key) && checkLetter(e.keyCode)) {
+                if (secretWord.includes(letter)) {
+                    addCorrectLetter(secretWord.indexOf(letter));
+                    for (let i = 0; i < secretWord.length; i++) {
+                        if (secretWord[i] === letter) {
+                            writeCorrectLetter(i);
+                            checkWinner(letter);
+                        }
+                    }
+                } else {
+                    if (!checkLetterClicked(e.key) && !checkWinner(letter))
+                        return;
+                    drawHanged(errors);
+                    checkEndGame(letter);
                 }
             }
         } else {
-            addIncorrectLetter(letter);
-            writeIncorrectLetter(letter, errors);
+            alert("has atingido el límite de letras incorrectas");
         }
     };
 }
